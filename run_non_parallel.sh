b27028e8997f190183ae5073fe1fcd62fe1500a4
jeffskwang <30662804+jeffskwang@users.noreply.github.com>
2022-06-15 12:34:20 -0500

    update to single run

--- run_non_parallel.sh
+++ run_non_parallel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for i in {1..410}
-do
-	echo "python SOC_LEM_County.py $i"
-done
+python SOC_LEM_County.py 1
+python SOC_LEM_County.py 2
+python SOC_LEM_County.py 3
+python SOC_LEM_County.py 4
+python SOC_LEM_County.py 5
